1d2e9cfecab8aa91177aa0458aa336430cb3fe8d
Guido Eckelt <guido.eckelt@gmail.com>
2017-01-13 12:04:58 +0100

    version2

---
+++ Version2/build.cmd
@@ -0,0 +1,4 @@
+electron-packager . ElectronApp --platform=win32 --arch=ia32 --out=dist --overwrite --asar
+#electron-packager src ElectronApp2 --platform=win32 --arch=ia32 --out=dist --overwrite --asar
+#npm run build
+pause >nul
